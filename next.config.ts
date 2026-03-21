473f81ea119783051962c668e7d52c1894ad2bf9
Vivek Vani <vivekvanisrd@gmail.com>
2026-03-21 18:52:38 +0530

    fix_build_safeguards

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-// Trigger reload: 2026-03-15T00:32:00
+// Trigger reload: 2026-03-21T18:10:00
 const nextConfig: NextConfig = {
   /* config options here */
 };
